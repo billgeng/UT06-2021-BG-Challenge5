8607fdab595b79f1fcb9264ad83ed30ac8eac87e
bill geng <billgeng@yahoo.com>
2021-07-26 12:01:28 -0400

    change js for

--- assets/script.js
+++ assets/script.js
@@ -3,126 +3,188 @@ var timer = document.querySelector('#timer');
 timer.innerText = timerCurrent;
 
 const schedulerText9Input = document.querySelector('#text-9');
-const text = [];
-const submitButton = document.querySelector('#submitBtn');
-const storedInput = localStorage.getItem('scheduleinput');
+const text9 = [];
+const submitButton9 = document.querySelector('#submitBtn9');
+const storedInput9 = localStorage.getItem('scheduleinput9');
+
 
 
 schedulerText9Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text9.textContent = schedule.target.value;
     
 })
 
-
-const saveToLocalStorage = () => {
-    localStorage.setItem('scheduleinput', text.textContent);
+const saveToLocalStorage9 = () => {
+    localStorage.setItem('scheduleinput9', text9.textContent);
 }
 
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-9').innerHTML = storedInput; 
+submitButton9.addEventListener('click',saveToLocalStorage9);
+document.getElementById('#text-9').innerHTML = storedInput9; 
 
 const schedulerText10Input = document.querySelector('#text-10');
+const text10 = [];
+const submitButton10 = document.querySelector('#submitBtn10');
+const storedInput10 = localStorage.getItem('scheduleinput10');
+
 
 
 schedulerText10Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text10.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage10 = () => {
+    localStorage.setItem('scheduleinput10', text10.textContent);
+}
+
+submitButton10.addEventListener('click',saveToLocalStorage10);
+document.getElementById('#text-10').innerHTML = storedInput10; 
 
 
-
-submitButton.addEventListener('click',saveToLocalStorage);
-
-document.getElementById('#text-10').innerHTML = storedInput; 
 
 
 const schedulerText11Input = document.querySelector('#text-11');
+const text11 = [];
+const submitButton11 = document.querySelector('#submitBtn11');
+const storedInput11 = localStorage.getItem('scheduleinput11');
+
 
 
 schedulerText11Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text11.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage11 = () => {
+    localStorage.setItem('scheduleinput11', text11.textContent);
+}
+
+submitButton11.addEventListener('click',saveToLocalStorage11);
+document.getElementById('#text-11').innerHTML = storedInput11; 
 
 
 
-submitButton.addEventListener('click',saveToLocalStorage);
 
-document.getElementById('#text-11').innerHTML = storedInput; 
 
 const schedulerText12Input = document.querySelector('#text-12');
+const text12 = [];
+const submitButton12 = document.querySelector('#submitBtn12');
+const storedInput12 = localStorage.getItem('scheduleinput12');
+
+
 
 schedulerText12Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text12.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage12 = () => {
+    localStorage.setItem('scheduleinput12', text12.textContent);
+}
 
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-12').innerHTML = storedInput; 
+submitButton12.addEventListener('click',saveToLocalStorage12);
+document.getElementById('#text-12').innerHTML = storedInput12; 
+
 
 const schedulerText13Input = document.querySelector('#text-13');
+const text13 = [];
+const submitButton13 = document.querySelector('#submitBtn13');
+const storedInput13 = localStorage.getItem('scheduleinput13');
+
+
 
 schedulerText13Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text13.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage13 = () => {
+    localStorage.setItem('scheduleinput13', text13.textContent);
+}
 
+submitButton13.addEventListener('click',saveToLocalStorage13);
+document.getElementById('#text-13').innerHTML = storedInput13; 
 
-
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-13').innerHTML = storedInput; 
 
 const schedulerText14Input = document.querySelector('#text-14');
+const text14 = [];
+const submitButton14 = document.querySelector('#submitBtn14');
+const storedInput14 = localStorage.getItem('scheduleinput14');
+
 
 
 schedulerText14Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text14.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage14 = () => {
+    localStorage.setItem('scheduleinput14', text14.textContent);
+}
+
+submitButton14.addEventListener('click',saveToLocalStorage14);
+document.getElementById('#text-14').innerHTML = storedInput14; 
 
 
-
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-14').innerHTML = storedInput; 
 
 const schedulerText15Input = document.querySelector('#text-15');
+const text15 = [];
+const submitButton15 = document.querySelector('#submitBtn15');
+const storedInput15 = localStorage.getItem('scheduleinput15');
+
 
 
 schedulerText15Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text15.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage15 = () => {
+    localStorage.setItem('scheduleinput15', text15.textContent);
+}
 
+submitButton15.addEventListener('click',saveToLocalStorage15);
+document.getElementById('#text-15').innerHTML = storedInput15; 
 
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-15').innerHTML = storedInput; 
 
 const schedulerText16Input = document.querySelector('#text-16');
+const text16 = [];
+const submitButton16 = document.querySelector('#submitBtn16');
+const storedInput16 = localStorage.getItem('scheduleinput16');
+
+
 
 schedulerText16Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text16.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage16 = () => {
+    localStorage.setItem('scheduleinput16', text16.textContent);
+}
 
-submitButton.addEventListener('click',saveToLocalStorage);
-document.getElementById('#text-16').innerHTML = storedInput; 
+submitButton16.addEventListener('click',saveToLocalStorage16);
+document.getElementById('#text-16').innerHTML = storedInput16; 
+
+
 
 const schedulerText17Input = document.querySelector('#text-17');
+const text17 = [];
+const submitButton17 = document.querySelector('#submitBtn17');
+const storedInput17 = localStorage.getItem('scheduleinput17');
+
+
 
 schedulerText17Input.addEventListener('input',schedule => {
-    text.textContent = schedule.target.value;
+    text17.textContent = schedule.target.value;
     
 })
 
+const saveToLocalStorage17 = () => {
+    localStorage.setItem('scheduleinput17', text17.textContent);
+}
 
-submitButton.addEventListener('click',saveToLocalStorage);
+submitButton17.addEventListener('click',saveToLocalStorage17);
+document.getElementById('#text-17').innerHTML = storedInput17; 
 
-document.getElementById('#text-17').innerHTML = storedInput; 
 
